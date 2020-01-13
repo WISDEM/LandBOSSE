6678428035452b02941a7ed3565c7da407e1ba34
Alicia Key <alicia.key@nrel.gov>
2020-01-12 17:04:53 -0700

    Update generate_extended_landbosse_output.py
    ETL works!

--- post_processing_scripts/generate_extended_landbosse_output.py
+++ post_processing_scripts/generate_extended_landbosse_output.py
@@ -1,5 +1,10 @@
 import os
+import io
+
+import psycopg2
+from sqlalchemy import create_engine
 import pandas as pd
+
 
 landbosse_output_path = "landbosse-output.xlsx"
 landbosse_extended_project_list_path = os.path.join("calculated_parametric_inputs", "extended_project_list.xlsx")
@@ -22,3 +27,26 @@ join_landbosse_output_details = landbosse_output_details.merge(right=extended_pr
 print("Writing .csv files...")
 join_landbosse_output_costs.to_csv("extended_landbosse_costs.csv", index=False)
 join_landbosse_output_details.to_csv("extended_landbosse_details.csv", index=False)
+
+etl_into_database_enabled = True
+if etl_into_database_enabled:
+    print("ETL into database...")
+    pg_password = os.environ.get("PG_PASSWORD", "no postgresql pasword was specified")
+    pg_user = os.environ.get("PG_USER", "no postgresql pasword was specified")
+    pg_database = os.environ.get("PG_DATABASE", "no pg database was specified")
+    pg_port = os.environ.get("PG_PORT", "no pg port was specified")
+    pg_host = os.environ.get("PG_HOST", "localhost")
+    pg_port = os.environ.get("PG_PORT", "5432")
+    engine_string = f"postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_database}"
+    engine = create_engine(engine_string)
+    print(f"Using engine {engine}")
+    table_name = "extended_project_list_with_costs"
+    join_landbosse_output_costs.head(0).to_sql(table_name, engine, if_exists="replace", index=False)
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    output = io.StringIO()
+    join_landbosse_output_costs.to_csv(output, sep='\t', header=False, index=False)
+    output.seek(0)
+    contents = output.getvalue()
+    cur.copy_from(output, table_name, null="")
+    conn.commit()
