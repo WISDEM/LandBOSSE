02f43b5a74f4b43e8c17cb8360a760446d2a98f2
Annika Eberle <annika.eberle@nrel.gov>
2020-01-15 14:05:33 -0700

    Adds rate of deliveries to auto-scaling

--- landbosse/excelio/XlsxReader.py
+++ landbosse/excelio/XlsxReader.py
@@ -563,6 +563,7 @@ class XlsxReader:
         project_size_MW = project_parameters['Number of turbines'] * project_parameters['Turbine rating MW']
         hub_height_m = project_parameters['Hub height m']
         flag_use_user_homerun = project_parameters['Flag for user - defined home run trench length (0 = no; 1 = yes)']
+        nameplate = project_parameters['Turbing rating MW']
 
         distance_to_interconnect_mi = 0.0 if project_size_MW <= 20 else (0.009375 * project_size_MW + 0.625)
         interconnect_voltage_kV = 0.4398 * project_size_MW + 60.204
@@ -573,10 +574,12 @@ class XlsxReader:
         number_of_highway_permits = ceil(0.2 * project_parameters['Number of turbines'])
         if flag_use_user_homerun is 1:
              user_input_homerun_km = 0.1776 * project_size_MW - 2.551
+        rate_deliveries = ceil(15 / nameplate)  # 10 deliveries per week for 1.5 MW machines
 
         # $17,000 / MW for a development cost estimate
         development_labor_cost_usd = project_size_MW * 17000
 
+        project_parameters['Rate of deliveries(turbines per week)'] = rate_deliveries
         project_parameters['Combined Homerun Trench Length to Substation (km)'] = user_input_homerun_km
         project_parameters['Development labor cost USD'] = development_labor_cost_usd
         project_parameters['Project size MW'] = project_size_MW
