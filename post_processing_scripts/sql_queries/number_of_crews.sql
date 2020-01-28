f967e8904c75e835ad74b0ed77d8379709d70b2c
Alicia Key <alicia.key@nrel.gov>
2020-01-28 11:48:51 -0700

    Create number_of_crews.sql
    This query is for finding the number of crews by dividing the time needed for construction by the time available for construction.

---
+++ post_processing_scripts/sql_queries/number_of_crews.sql
@@ -0,0 +1,23 @@
+SELECT
+	"Project ID with serial",
+	"Module",
+	"name",
+	"unit",
+	"Numeric value",
+	"Non-numeric value",
+	"Total project construction time (months)",
+	"Number of turbines",
+	"Turbine rating MW",
+	"Turbine rating MW" * "Number of turbines" AS "Plant size MW",
+	"Hub height m",
+	"Labor cost multiplier" AS "Labor cost",
+	"Crane breakdown fraction" AS "Crane break"
+FROM 
+	details_with_extended_project_list
+WHERE
+	"name" = 'erection_selected_detailed_data: Operation-Crane name-Boom system-Number of crews'
+ORDER BY 
+	"Project ID with serial",
+	"Module",
+	"name"
+LIMIT 100;
