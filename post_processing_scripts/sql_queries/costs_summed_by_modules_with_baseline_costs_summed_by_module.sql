3a320ca3c8002b05826f6c79163b5e2a7d6314c1
Alicia Key <alicia.key@nrel.gov>
2020-02-04 08:40:03 -0700

    Update costs_summed_by_modules_with_baseline_costs_summed_by_module.sql
    This SQL enables parametric exploration of variable baselines.

--- post_processing_scripts/sql_queries/costs_summed_by_modules_with_baseline_costs_summed_by_module.sql
+++ post_processing_scripts/sql_queries/costs_summed_by_modules_with_baseline_costs_summed_by_module.sql
@@ -1,51 +1,50 @@
 WITH baseline_costs_summed_by_module AS (SELECT
-	 SUM("Cost per turbine") AS "$/turbine baseline summed by module",
-	 SUM("Cost per project") AS "$/project baseline summed by module",
-	 SUM("USD/kW per project") AS "$/kW baseline summed by module",
+	 "Cost per turbine" AS "$/turbine baseline",
+	 "Cost per project" AS "$/project baseline",
+	 "USD/kW per project" AS "$/kW baseline",
 	 "Project ID with serial" AS id,
 	 "Module",
-	 "Turbine rating MW",
-	 "Hub height m",
-	 "Labor cost multiplier" AS "Labor rate",
-	 "Crane breakdown fraction" AS "Crane break",
-	 ROUND("Number of turbines"::numeric * "Turbine rating MW"::numeric, -1) AS "Plant size MW"
+	 "Type of cost",
+	 "Turbine rating MW" AS "Baseline turbine rating MW",
+	 "Hub height m" AS "Baseline hub height m",
+	 "Labor cost multiplier" AS "Baseline labor rate",
+	 "Crane breakdown fraction" AS "Baseline crane break",
+	 ROUND("Number of turbines"::numeric * "Turbine rating MW"::numeric, -1) AS "Baseline plant size MW"
 FROM
 	costs_with_extended_project_list
 WHERE
 	ROUND("Number of turbines"::numeric * "Turbine rating MW"::numeric, -1) = 150
-	AND "Hub height m" = 90
+	AND "Hub height m" = 110
 	AND "Turbine rating MW" = 2.5
 	AND "Crane breakdown fraction" = 0
 	AND "Labor cost multiplier" = 1.0
-GROUP BY 4, 5, 6, 7, 8, 9, 10
-ORDER BY 4, 5)
+ORDER BY "Project ID with serial", "Module", "Type of cost")
 
 SELECT
 	c."Project ID with serial" AS id,
-	 SUM(c."Cost per turbine") AS "$/turbine summed by module",
-	 SUM(c."Cost per project") AS "$/project summed by module",
-	 SUM(c."USD/kW per project") AS "$/kW summed by module",
-	 b."$/turbine baseline summed by module",
-	 b."$/project baseline summed by module",
-	 b."$/kW baseline summed by module",
+	 c."Cost per turbine" AS "$/turbine",
+	 c."Cost per project" AS "$/project",
+	 c."USD/kW per project" AS "$/kW",
+	 b."$/turbine baseline",
+	 b."$/project baseline",
+	 b."$/kW baseline",
 	 c."Module",
+	 c."Type of cost",
 	 c."Turbine rating MW",
  	 c."Hub height m",
  	 c."Labor cost multiplier" AS "Labor rate",
  	 c."Crane breakdown fraction" AS "Crane break",
  	 ROUND(c."Number of turbines"::numeric * c."Turbine rating MW"::numeric, -1) AS "Plant size MW",
-	 b."Turbine rating MW" AS "Baseline turbine rating MW",
-	 b."Hub height m" AS "Baseline hub height m",
-	 b."Labor rate" AS "Baseline labor rate",
-	 b."Crane break" AS "Baseline crane break",
-	 b."Plant size MW" AS "Baseline plant size MW"
+	 b."Baseline turbine rating MW",
+	 b."Baseline hub height m",
+	 b."Baseline labor rate",
+	 b."Baseline crane break",
+	 b."Baseline plant size MW"
 FROM
 	costs_with_extended_project_list c
 JOIN
 	baseline_costs_summed_by_module b
-	ON c."Module" = b."Module"
-GROUP BY 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
+	ON c."Module" = b."Module" AND c."Type of cost" = b."Type of cost"
 ORDER BY
 	c."Project ID with serial",
-	c."Module"
-LIMIT 100;
+	c."Module";
