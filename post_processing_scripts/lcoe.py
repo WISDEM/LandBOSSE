8c2c2823136ea755a25abd2d2e210da469ad7300
Alicia Key <akey7@users.noreply.github.com>
2020-03-12 19:44:28 -0600

    Issue #129
    Put diagnostic logging.

--- post_processing_scripts/lcoe.py
+++ post_processing_scripts/lcoe.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # Select every row from the LandBOSSE output.
     # Only select the needed columns, convert MW to kW and rename the columns
     # to be consistent with the AEP and TCC data.
-    bos = pd.read_excel('landbosse-output.xlsx', 'costs_by_module_type_operation')
+    bos = pd.read_csv('extended_landbosse_costs.csv')
     bos = bos[['Number of turbines', 'Turbine rating MW', 'Rotor diameter m', 'Cost per project']]
     bos['Rating [kW]'] = bos['Turbine rating MW'] * 1000
     bos.rename(columns={'Rotor diameter m': 'Rotor Diam [m]', 'Cost per project': 'BOS Capex [USD]'}, inplace=True)
@@ -16,15 +16,18 @@ if __name__ == '__main__':
 
     # Aggregate and sum BOS costs
     bos_sum = bos.groupby(['Rating [kW]', 'Rotor Diam [m]', 'Number of turbines']).sum().reset_index()
+    print(f"bos_sum: {len(bos_sum)}")
 
     # Inner join AEP and TCC. Taken together, Rating [kW] and Rotor Diam [m]
     # are the key.
     aep_tcc = aep.merge(tcc, on=['Rating [kW]', 'Rotor Diam [m]'])
+    print(f"aep_tcc: {len(aep_tcc)}")
 
     # Then join in the BOS sum data, again using Rating [kW] and
     # Rotor Diam [m] as keys. This dataframe will eventually have the
     # LCOE as a column.
     lcoe = aep_tcc.merge(bos_sum, on=['Rating [kW]', 'Rotor Diam [m]'])
+    print(f"lcoe: {len(lcoe)}")
 
     # Create columns for FCR and Opex USD/kW
     lcoe['FCR'] = 0.079
