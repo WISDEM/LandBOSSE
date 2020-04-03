5fd705b7cdfb5345f24c805c9d1d35abbac6a86b
parangat94 <parangat.bhaskar94@gmail.com>
2020-04-03 14:34:47 -0600

    Add 8 tests to run_landbosse(). These test the individual total cost buckets

--- landbosse/landbosse_api/API_test.py
+++ landbosse/landbosse_api/API_test.py
@@ -1,27 +1,51 @@
-import unittest
+from landbosse.landbosse_api.run import *
 from unittest import TestCase
+
 
 
 class TestLandBOSSE_API(TestCase):
     def setUp(self):
-        api_inputs = dict()
-        api_inputs['interconnect_voltage_kV'] = 137
-        api_inputs['distance_to_interconnect_mi'] = 10
-        api_inputs['num_turbines'] = 100
-        api_inputs['turbine_spacing_rotor_diameters'] = 4
-        api_inputs['row_spacing_rotor_diameters'] = 10
-        api_inputs['turbine_rating_MW'] = 1.5
-        api_inputs['rotor_diameter_m'] = 77
-        api_inputs['hub_height_meters'] = 80
-        api_inputs['wind_shear_exponent'] = 0.20
-        api_inputs['depth'] = 2.36  # Foundation depth in m
-        api_inputs['rated_thrust_N'] = 589000
-        api_inputs['labor_cost_multiplier'] = 1
-        api_inputs['gust_velocity_m_per_s'] = 59.50
+        self.api_inputs = dict()
+        self.api_inputs['interconnect_voltage_kV'] = 137
+        self.api_inputs['distance_to_interconnect_mi'] = 10
+        self.api_inputs['num_turbines'] = 100
+        self.api_inputs['turbine_spacing_rotor_diameters'] = 4
+        self.api_inputs['row_spacing_rotor_diameters'] = 10
+        self.api_inputs['turbine_rating_MW'] = 1.5
+        self.api_inputs['rotor_diameter_m'] = 77
+        self.api_inputs['hub_height_meters'] = 80
+        self.api_inputs['wind_shear_exponent'] = 0.20
+        self.api_inputs['depth'] = 2.36  # Foundation depth in m
+        self.api_inputs['rated_thrust_N'] = 589000
+        self.api_inputs['labor_cost_multiplier'] = 1
+        self.api_inputs['gust_velocity_m_per_s'] = 59.50
+        self.results = run_landbosse(self.api_inputs)
+        print(self.results)
 
     def test_total_BOS_cost(self):
-        self.assertEqual(True, False)
+        self.assertEqual(self.results['total_bos_cost'], 44648532.791873954)
+
+    def test_total_management_cost(self):
+        self.assertEqual(self.results['total_management_cost'], 10616123.290804042)
+
+    def test_total_siteprep_cost(self):
+           self.assertEqual(self.results['total_sitepreparation_cost'], 3195070.1679882305)
+
+    def test_total_foundation_cost(self):
+           self.assertEqual(self.results['total_foundation_cost'], 10411261.424160697)
 
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_total_erection_cost(self):
+           self.assertEqual(self.results['total_erection_cost'], 5231110.226456955)
+
+    def test_total_gridconnection_cost(self):
+           self.assertEqual(self.results['total_gridconnection_cost'], 5617735.863912535)
+
+
+    def test_total_collection_cost(self):
+           self.assertEqual(self.results['total_collection_cost'], 4869435.746468595)
+
+    def test_total_substation_cost(self):
+           self.assertEqual(self.results['total_substation_cost'], 4940746.072082901)
+
+
