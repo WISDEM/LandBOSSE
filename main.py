86ddabe32420be9ae4ec950901b7e027c8ba0ddf
Alicia Key <akey7@users.noreply.github.com>
2020-04-28 09:32:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     # returns an exit code of either 0 (successful validation), or 1 (validation failed).
     #
     # If validation was not enabled, exit with a status of 0 (no errors)
-    
+
     if validation_enabled:
         exit(build_status)
     else:
