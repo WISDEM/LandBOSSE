114aab6093b99e9f3fe7a99585759e99b39b2f26
Bhaskar <parangat.bhaskar@nrel.gov>
2021-03-02 19:30:17 -0700

    Update pytest inputs to get unit tests to pass

--- landbosse/tests/model/test_SubstationCost.py
+++ landbosse/tests/model/test_SubstationCost.py
@@ -14,11 +14,11 @@ class TestSubstationCost(TestCase):
         self.project_name = 'Project_1'
         self.output_dict = dict()
 
-        # self.input_dict['turbine_rating_MW'] = 1.5
-        # self.input_dict['num_turbines'] = 15
-        # self.input_dict['project_size_megawatts'] = self.input_dict[
-        #              'num_turbines'] * self.input_dict['turbine_rating_MW']  # MW
-        # self.input_dict['rotor_diameter_m'] = 75
+        self.input_dict['turbine_rating_MW'] = 1.5
+        self.input_dict['num_turbines'] = 15
+        self.input_dict['project_size_megawatts'] = self.input_dict[
+                     'num_turbines'] * self.input_dict['turbine_rating_MW']  # MW
+        self.input_dict['rotor_diameter_m'] = 75
 
     def test_SubstationCostModule(self):
         """
