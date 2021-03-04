Fixed voltage units

landbosse/model/CollectionCost.py
@@ -38,7 +38,7 @@ class Cable:
         current_capacity : float
             Cable current rating at 1m burial depth, Amps
         rated_voltage : float
-            Cable rated voltage, V
+            Cable rated voltage, kV
         ac_resistance : float
             Cable resistance for AC current, Ohms/km
         inductance : float
@@ -117,7 +117,7 @@ class Cable:
         """
 
         # TODO: Verify eqn is correct
-        self.cable_power = (np.sqrt(3) * self.rated_voltage * self.current_capacity * self.power_factor / 1000)
+        self.cable_power = (np.sqrt(3) * self.rated_voltage*1000 * self.current_capacity * self.power_factor / 1000)
 
 
 class Array(Cable):
@@ -303,11 +303,11 @@ class ArraySystem(CostModule):
 
     def calc_current_properties(self):
         """
-        Find collection system voltage [V] and turbine capacity [MW]. Sort cables by current capacity.
+        Find collection system voltage [kV] and turbine capacity [MW]. Sort cables by current capacity.
 
         Returns
         -------
-        self.collection_V: collection system voltage [V]
+        self.collection_V: collection system voltage [kV]
         self.turbine_capacity: turbine capacity [MW] at collection system voltage
         """
 
@@ -317,13 +317,14 @@ class ArraySystem(CostModule):
         self.collection_V = 9999
 
         for cable, property in self.input_dict['cable_specs_pd'].head().iterrows():
-            if property['Rated Voltage (V)'] < self.collection_V:
-                self.collection_V = property['Rated Voltage (V)']
+            rated_voltage_kV = property['Rated Voltage (V)'] * 1000
+            if rated_voltage_kV > self.collection_V:
+                self.collection_V = rated_voltage_kV
 
         # sort cables by current capacity
         self.input_dict['cable_specs_pd'].sort_values(by=['Current Capacity (A)'], inplace=True)
         self.input_dict['cable_specs_pd'] = self.input_dict['cable_specs_pd'].reset_index(drop=True)
-        self.turbine_power = self.input_dict['turbine_rating_MW'] #* 1e3 / self.collection_V
+        self.turbine_power = self.input_dict['turbine_rating_MW']#/ self.collection_V
 
     def calc_required_segment_capacity(self):
         """
@@ -373,7 +374,7 @@ class ArraySystem(CostModule):
                 array_dict['cable' + str(k)]['End point'] = self.L[j, :]
                 array_dict['cable' + str(k)]['Length'] = ((self.L[i, 0] - self.L[j, 0]) ** 2 + (
                         self.L[i, 1] - self.L[j, 1]) ** 2) ** (1 / 2)
-                array_dict['cable' + str(k)]['Power'] = min(self.C[i], self.C[j])
+                array_dict['cable' + str(k)]['Power Capacity'] = min(self.C[i], self.C[j])
                 array_dict['cable' + str(k)]['Terminal?'] = False
                 k += 1  # iterate to make new cable
             remains[i] = False  # prevent duplicate cables
@@ -381,7 +382,7 @@ class ArraySystem(CostModule):
         array_dict['cable' + str(k)] = dict()
         array_dict['cable' + str(k)]['Length'] = self.input_dict[
                                                      'distance_to_grid_connection_km'] * 5280 * self._km_to_LF
-        array_dict['cable' + str(k)]['Power'] = self.n_segments * self.turbine_power
+        array_dict['cable' + str(k)]['Power Capacity'] = self.n_segments * self.turbine_power
         array_dict['cable' + str(k)]['Terminal?'] = True
         self.array_dict = array_dict
 
@@ -796,13 +797,13 @@ class ArraySystem(CostModule):
          dissipated_power = 0    #W
          for segment in self.array_dict:
              for idx, (name, cable) in enumerate(self.cables.items()):
-                 if cable.current_capacity >= self.array_dict[segment]['Power']:
+                 if cable.current_capacity >= self.array_dict[segment]['Power Capacity']:
                      cable.total_length += self.array_dict[segment]['Length']
                      self.output_dict['total_cable_len_km'] += self.array_dict[segment]['Length']
                      # cable.total_mass = cable.total_length * cable.mass
                      cable.total_cost = (cable.total_length / self._km_to_LF) * cable.cost
                      self._total_cable_cost += (self.array_dict[segment]['Length'] / self._km_to_LF) * cable.cost  # Keep running tally of total cable cost used in wind farm.
-                     dissipated_power += 3 * self.array_dict[segment]['Power']**2 * abs(cable.ac_resistance)*self.array_dict[segment]['Length']/1000    #TODO P=3*I^2*R. IF 3 phase. Divide by 1000 to go from ohm/km->ohm/m
+                     dissipated_power += 3 * (self.array_dict[segment]['Power Capacity']*1e6/self.collection_V*1000)**2 * abs(cable.ac_resistance)*self.array_dict[segment]['Length']/1000    #TODO P=3*I^2*R. IF 3 phase. Divide by 1000 to go from ohm/km->ohm/m
                      break  # only assign one cable to each segment
 
          # add substation to transmission interconnect cable