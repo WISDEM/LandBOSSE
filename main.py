5fc385c7caebd1fb3975cd824493b11a531f4f6d
Bhaskar <parangat.bhaskar@nrel.gov>
2022-06-10 17:35:43 -0600

    Split nb into sections

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 from datetime import datetime
 import csv
-
+import matplotlib.pyplot as plt
 import pandas as pd
-from sympy import false
 
 from landbosse.excelio import XlsxSerialManagerRunner
 from landbosse.excelio import XlsxParallelManagerRunner
@@ -11,15 +10,24 @@ from landbosse.excelio import XlsxGenerator
 from landbosse.excelio import XlsxValidator
 from landbosse.excelio import CsvGenerator
 
+# When not running Binder and you want to provide your own input data, set_path to False:
+set_path = False
+
+# When running Binder set to True
+running_binder = False
+
 # LandBOSSE, small utility functions
 from landbosse.excelio import XlsxFileOperations
-set_path = False
-if set_path:
-    input_output_path = '~/Desktop/'
-    os.environ["LANDBOSSE_INPUT_DIR"] = input_output_path
-else:
-    input_output_path = os.path.dirname(__file__)
-    os.environ["LANDBOSSE_INPUT_DIR"] = input_output_path
+if running_binder == True:
+    binder_dir = os.path.join(os.getcwd(), 'jovyan')
+    input_output_path = os.path.dirname(binder_dir)
+else: 
+    if set_path:
+        input_output_path = '~/Desktop/'
+    else:
+        input_output_path = os.path.dirname(os.path.join(os.getcwd(), 'input'))
+
+os.environ["LANDBOSSE_INPUT_DIR"] = input_output_path
 
 
 if __name__ == '__main__':
@@ -29,30 +37,22 @@ if __name__ == '__main__':
     # The file_ops object handles file names for input and output data.
     file_ops = XlsxFileOperations()
 
-    # If run_parallel is True, an XlsxParallelManagerRunner will calculate the
-    # projects in parallel. This takes advantage of multicore architecture
-    # available on most hardware.
-    #
-    # If run_parallel is False, XlsxSerialManagerRunner will calculate projects
-    # serially. This is much slower so running in parallel is preferred
-    # unless there is a good reason to run serially. One such reason is using a
-    # debugger which can slow down when it is being used to debug multiple
-    # processes.
-
     run_parallel = False
     manager_runner = XlsxParallelManagerRunner(file_ops) if run_parallel else XlsxSerialManagerRunner(file_ops)
 
     # project_xlsx is the absolute path of the project_list.xlsx
     projects_xlsx = os.path.join(file_ops.landbosse_input_dir(), 'project_list.xlsx')
 
-    # Should we enable the scaling study? Just look for the scaling study
-    # option. The first three elements of the returned tuple aren't necessary
-    # to determine if the scaling study is enabled.
-    # Switch to either validation or non validation producing code.
-    input_path, output_path, validation_enabled, enable_scaling_study = file_ops.get_input_output_paths_from_argv_or_env()
-    # final_result aggregates all the results from all the projects.
-    final_result = manager_runner.run_from_project_list_xlsx(projects_xlsx, enable_scaling_study)
+    validation_enabled = False
     
+    # final_result aggregates all the results from all the projects.
+    final_result = manager_runner.run_from_project_list_xlsx(projects_xlsx, validation_enabled)
+
+    print('Simulation Successful!')
+    print('Results: ')
+    print('')
+
+    # print(final_result)
     total_collection_cost = final_result['jp_collectioncost_total']
     total_development_cost = final_result['jp_developmentcost_total']
     total_erection_cost = final_result['jp_erectioncost_total']
@@ -71,23 +71,44 @@ if __name__ == '__main__':
         total_management_cost + \
         total_sitepreparation_cost + \
         total_substation_cost
-
-    print(final_result['jp_collectioncost_total'])
-    print(final_result['jp_developmentcost_total'])
-    print(final_result['jp_erectioncost_total'])
-    print(final_result['jp_foundationcost_total'])
-    print(final_result['jp_gridconnectioncost_total'])
-    print(final_result['total_management_cost'])
-    print(final_result['jp_sitepreperationcost_total'])
-    print(final_result['jp_substationcost_total'])
-    print(total_bos_cost)
-
-
-    # returns an exit code of either 0 (successful validation), or 1 (validation failed).
-    #
-    # If validation was not enabled, exit with a status of 0 (no errors)
-    validation_enabled = False
-    if validation_enabled:
-        exit(build_status)
-    else:
-        exit(0)
+    
+    plant_capacity_kW = final_result['plant_capacity_MW'] * 1000
+        
+    total_bos_cost_kW = total_bos_cost / plant_capacity_kW
+    financial_costs_kW = 122 
+    turbine_capex_kW = 1021
+    
+    print('Plant Size MW: ', final_result['plant_capacity_MW'])
+    
+    print('Collection Cost ($/kW): ', round(total_collection_cost / plant_capacity_kW))
+    print('Development Cost ($/kW): ', round(total_development_cost / plant_capacity_kW))
+    print('Erection Cost ($/kW): ', round(total_erection_cost / plant_capacity_kW))
+    print('Foundation Cost ($/kW): ', round(total_foundation_cost / plant_capacity_kW))
+    print('Grid Connection Cost ($/kW): ', round(total_gridconnection_cost / plant_capacity_kW))
+    print('Management Cost ($/kW): ', round(total_management_cost / plant_capacity_kW))
+    print('Site Preparation Cost ($/kW): ', round(total_sitepreparation_cost / plant_capacity_kW))
+    print('Substation Cost ($/kW): ', round(total_substation_cost / plant_capacity_kW))
+    print('')
+    print('Total BOS CapEx ($/kW): ', round(total_bos_cost_kW))
+    print('Financial Costs ($/kW): ', financial_costs_kW)    
+    print('Turbine CapEx ($/kW): ', turbine_capex_kW)
+    print('Total Project CapEx ($/kW): ', round(total_bos_cost_kW + financial_costs_kW + turbine_capex_kW))
+    
+    # Plot
+    plt.figure(figsize=(10,10))
+    names = ['Turbine CapEx', 'Financial CapEx', 'BOS CapEx']
+    size = [turbine_capex_kW, financial_costs_kW, total_bos_cost_kW]
+    plt.pie(size, labels = names, 
+           startangle=90,
+           radius= 1.0, labeldistance=1.1,
+           textprops ={'fontweight': 'bold', 'fontsize':13, 'color': "black"},
+           wedgeprops = {'linewidth' : 3, 'edgecolor' : "black" }) 
+    
+    a = 'white'
+    plt.pie(size,startangle=90, pctdistance =1.25, autopct = '%1.1f%%',
+        radius= 0.60, colors=[a, a, a],
+       textprops ={'fontweight': 'bold' ,'fontsize':13, 'color': "white"}, 
+       wedgeprops = {'linewidth' : 3, 'edgecolor' : "black" } )
+ 
+    # show it
+    plt.show()
